08419cd08b8d08de67394e9a4b285fcf50a46959
heroLi <lixiao_1991@sina.com>
2014-11-03 10:40:35 +0800

    des
    des

---
+++ gen/com/mars/marsman/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mars.marsman;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/mars/marsman/app/MarsManApplication.java
@@ -0,0 +1,11 @@
+package com.mars.marsman.app;
+
+import android.app.Application;
+
+public class MarsManApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
