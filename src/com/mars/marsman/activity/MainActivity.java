ee32d04c02d157c76cda14598e3a9359aa0307c8
heroLi <lixiao_1991@sina.com>
2014-11-03 10:46:39 +0800

    1103

--- src/com/mars/marsman/activity/MainActivity.java
+++ src/com/mars/marsman/activity/MainActivity.java
@@ -1,14 +1,10 @@
 package com.mars.marsman.activity;
 
-import com.mars.marsman.R;
-import com.mars.marsman.R.id;
-import com.mars.marsman.R.layout;
-import com.mars.marsman.R.menu;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+
+import com.mars.marsman.R;
 
 public class MainActivity extends Activity {
 
@@ -25,15 +21,4 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
